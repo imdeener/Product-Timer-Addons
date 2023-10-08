02243cc63f878ad37bd1edb33c76ccf3c514ac63
Deener <imdeener@gmail.com>
2023-10-08 17:12:56 +0700

    Optimize Code

--- woopt-addons/js/script.js
+++ woopt-addons/js/script.js
@@ -1,25 +1,29 @@
 document.addEventListener("DOMContentLoaded", function() {
     var countdownElements = document.querySelectorAll('.woopt-countdown');
 
+    // Function สำหรับการอัพเดตเวลาถอยหลัง
+    function updateCountdown(countdownElement, endDate, interval) {
+        var now = Date.now();
+        var distance = endDate - now;
+
+        var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+        var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+        var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+        var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+        countdownElement.textContent = days + "d " + hours + "h " + minutes + "m " + seconds + "s ";
+
+        if (distance < 0) {
+            clearInterval(interval);
+            countdownElement.textContent = "EXPIRED";
+        }
+    }
+
     countdownElements.forEach(function(countdownElement) {
         var endDate = new Date(countdownElement.getAttribute('data-enddate')).getTime();
-
+        
         var interval = setInterval(function() {
-            // แก้ไขให้ใช้เวลา UTC+0 โดยการคำนวณจาก getTimezoneOffset
-            var now = new Date().getTime() + (new Date().getTimezoneOffset() * 60 * 1000);
-            var distance = endDate - now;
-
-            var days = Math.floor(distance / (1000 * 60 * 60 * 24));
-            var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-            var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
-            var seconds = Math.floor((distance % (1000 * 60)) / 1000);
-
-            countdownElement.innerHTML = days + "d " + hours + "h " + minutes + "m " + seconds + "s ";
-
-            if (distance < 0) {
-                clearInterval(interval);
-                countdownElement.innerHTML = "EXPIRED";
-            }
+            updateCountdown(countdownElement, endDate, interval);
         }, 1000);
     });
 });
